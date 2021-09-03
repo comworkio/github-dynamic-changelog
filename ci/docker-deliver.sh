e4c04efbc212634deb7a0d5ff77d181b72e55191
Idriss Neumann <idriss.neumann@comwork.io>
2021-09-03 12:22:41 +0100

    Fix push

--- ci/docker-deliver.sh
+++ ci/docker-deliver.sh
@@ -5,7 +5,6 @@ REPO_PATH="${BASE_DIR}/.."
 ARCH="${1}"
 IMAGE="${2}"
 VERSION="${3}"
-INTERNAL_VERSION="${4}"
 
 [[ $ARCH ]] || ARCH="x86"
 
@@ -27,18 +26,9 @@ COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f "${docker_compose
 
 echo "${DOCKER_ACCESS_TOKEN}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
 
-if [[ $ARCH == "x86" ]]; then
-  docker-compose -f docker-compose-build-${ARCH}.yml push "rollup_${ARCH}"
-  tag_and_push "${VERSION}" "${IMAGE}"
-  [[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}" "${IMAGE}"
-  tag_and_push "${VERSION}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
-  [[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
-fi
-
+[[ $ARCH == "x86" ]] && tag_and_push "latest" "${IMAGE}"
 tag_and_push "latest-${ARCH}" "${IMAGE}"
 tag_and_push "${VERSION}-${ARCH}" "${IMAGE}"
-[[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}-${ARCH}" "${IMAGE}"
 tag_and_push "${VERSION}-${ARCH}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
-[[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}-${ARCH}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
 
 exit 0
