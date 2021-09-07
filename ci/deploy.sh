60b956114e9defec6a219a2d6ac03b8d00e7a2ef
Idriss Neumann <idriss.neumann@comwork.io>
2021-09-07 11:55:58 +0100

    Rename dockerfile

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,4 +4,4 @@ REPO_PATH="/home/centos/github-dynamic-changelog/"
 
 cd "${REPO_PATH}" && git pull origin main || :
 echo "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN}" > .env
-docker-compose -f docker-compose-comwork.yml up -d --force-recreate
+docker-compose -f docker-compose-intra.yml up -d --force-recreate
