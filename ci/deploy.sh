2799d5738ae43d5f8dc9b544ad85c46c1b6d850e
Idriss Neumann <neumann.idriss@gmail.com>
2021-12-26 13:40:54 +0000

    Update ci/deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-REPO_PATH="${PROJECT_HOME}/github-dynamic-changelog/"
-
-cd "${REPO_PATH}" && git pull origin main || :
 echo "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN}" > .env
+docker rmi -f "comworkio/github-dynamic-changelog:latest" || :
 docker-compose -f docker-compose-intra.yml up -d --force-recreate
