bec8518a82e9486ae461df4b72a59525b32476b2
Idriss Neumann <idriss.neumann@comwork.io>
2022-03-07 10:16:23 +0100

    Fix: cast problem

--- api/common_utils.py
+++ api/common_utils.py
@@ -4,11 +4,11 @@ def is_not_empty (var):
     elif (isinstance(var, int)):
         return False
     empty_chars = ["", "null", "nil", "false", "none"]
-    return var is not None and not any(c == var.lower() for c in empty_chars)
+    return var is not None and not any(c == "{}".format(var).lower() for c in empty_chars)
 
 def is_true (var) :
     false_char = ["false", "ko", "no"]
-    return is_empty(var) or not any(c == var.lower() for c in false_char)
+    return is_empty(var) or not any(c == "{}".format(var).lower() for c in false_char)
 
 def is_empty (var):
     return not is_not_empty(var)
