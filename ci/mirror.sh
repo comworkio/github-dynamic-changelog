83546ee8621d0696e3556944ae0c4d081d788271
Idriss Neumann <idriss.neumann@comwork.io>
2023-01-04 16:18:47 +0100

    Remove some mirrors and add dockerignore

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -4,8 +4,5 @@ REPO_PATH="${PROJECT_HOME}/github-dynamic-changelog/"
 
 cd "${REPO_PATH}" && git pull origin main || :
 git push github main 
-git push internal main
 git push pgitlab main
-git push bitbucket main
-git push froggit main
 exit 0
